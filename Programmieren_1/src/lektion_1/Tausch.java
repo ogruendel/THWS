ad25d4723f0a64b45a1c3f30010393859ed626bb
Oliver <oliver.gruendel2004@gmail.com>
2023-10-07 15:13:05 +0200

    Lektion 1: Tausch

---
+++ Programmieren_1/src/lektion_1/Tausch.java
@@ -0,0 +1,21 @@
+package lektion_1;
+
+public class Tausch {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        //Vor dem Tausch
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        //Tausch
+        int temp = x;
+        x = y;
+        y = temp;
+
+        //Nach dem Tausch
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
