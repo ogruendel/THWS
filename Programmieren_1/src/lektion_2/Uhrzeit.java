b37f213c1c66fd4a46377859a93c7b28982f18ca
Oliver <oliver.gruendel2004@gmail.com>
2023-10-09 13:20:22 +0200

    Lektion 2: Uhrzeit

---
+++ Programmieren_1/src/lektion_2/Uhrzeit.java
@@ -0,0 +1,25 @@
+package lektion_2;
+
+import java.time.LocalTime;
+
+public class Uhrzeit {
+    public static void main(String[] args) {
+
+        int SECONDS_PER_DAY = 86400;
+        int SECONDS_PER_HOUR = 3600;
+        int SECONDS_PER_MINUTE = 60;
+
+        int hour = LocalTime.now().getHour();
+        int minute = LocalTime.now().getMinute();
+        int second = LocalTime.now().getSecond();
+
+        int secSinceMidnight = hour * SECONDS_PER_HOUR + minute * SECONDS_PER_MINUTE + second;
+        int secRemaining = SECONDS_PER_DAY - secSinceMidnight;
+
+        float percentage = ((float) secSinceMidnight / (float) SECONDS_PER_DAY) * 100f;
+
+        System.out.println("Vergangene Sekunden seit Mitternacht: " + secSinceMidnight);
+        System.out.println("Sekunden heute verbleibend: " + secRemaining);
+        System.out.println("Prozent vom Tag vorbei: " + percentage);
+    }
+}
