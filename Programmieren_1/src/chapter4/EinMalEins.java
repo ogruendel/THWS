e2807cadb098e42028a6d46e399363d70e0a467d
Oliver <oliver.gruendel2004@gmail.com>
2023-10-24 09:38:36 +0200

    Chapter 4: Ein mal Eins

---
+++ Programmieren_1/src/chapter4/EinMalEins.java
@@ -0,0 +1,11 @@
+package chapter4;
+
+public class EinMalEins {
+    public static void main(String[] args) {
+        for (int num1 = 1; num1 <= 10; num1++) {
+            for (int num2 = 1; num2 <= 10; num2++) {
+                System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+            }
+        }
+    }
+}
