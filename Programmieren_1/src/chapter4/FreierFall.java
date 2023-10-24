17594dd35e5fd38b568af2d652049cffe3325c8a
Oliver <oliver.gruendel2004@gmail.com>
2023-10-24 09:45:43 +0200

    Chapter 4: FreierFall

---
+++ Programmieren_1/src/chapter4/FreierFall.java
@@ -0,0 +1,30 @@
+package chapter4;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class FreierFall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        final double G = 9.80665;
+
+        System.out.println("Bitte geben sie eine maximale Falldauer in Sekunden ein:");
+        int tMax = scanner.nextInt();
+
+        /*for (int t = 1; t <= tMax; t++) {
+            double s = 0.5 * G * t * t;
+            System.out.println(t + "s: " + df.format(s) + "m");
+        }*/
+
+        for (int t = 1; t <= tMax; t++) {
+            if ((t - 1) % 5 == 0) {
+                double s = 0.5 * G * t * t;
+                System.out.println(t + "s: " + df.format(s) + "m");
+            }
+        }
+
+        scanner.close();
+    }
+}
