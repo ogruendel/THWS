6e6e211a52f6f1f2907c8b14b10b20794558b047
Oliver <oliver.gruendel2004@gmail.com>
2023-10-24 09:24:33 +0200

    Chapter 4: Temperaturtabelle

---
+++ Programmieren_1/src/chapter4/Temperaturtabelle.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+import java.text.DecimalFormat;
+
+public class Temperaturtabelle {
+    public static void main(String[] args) {
+        int max = 300;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("Fahrenheit       Celsius");
+        for (int fahrenheit = 0; fahrenheit <= max; fahrenheit++) {
+            double celsius = (5.0/9.0) * (fahrenheit - 32);
+            System.out.println(fahrenheit + "               " + df.format(celsius));
+        }
+    }
+}
