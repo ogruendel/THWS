375fd83d2816b4ce32c8d69b494043c973879ff0
Oliver <oliver.gruendel2004@gmail.com>
2023-10-31 10:57:38 +0100

    Chapter 5: Reihenwert

---
+++ Programmieren_1/src/chapter5/Reihenwert.java
@@ -0,0 +1,28 @@
+package chapter5;
+
+public class Reihenwert {
+    public static void main(String[] args) {
+        final double cutoff = 1E-5;
+        double deltaSum;
+        double prev = 0;
+
+        int k = 1;
+        double sum = 0;
+
+        do {
+            sum += 1.0 / (k * k);
+
+            deltaSum = Math.abs(prev - sum);
+            prev = sum;
+
+            System.out.println(6 * sum);
+            k++;
+        } while (deltaSum >= cutoff);
+    }
+
+    /*
+    zu c:
+          Wenn der Faktor 6 mit in der Summe steht, dann wird jeder einzelne Term 1 / k² versechsfacht.
+          Steht der Faktor 6 vor der Summe, wird nur das Endergebnis versechsfacht.
+     */
+}
