3a98209535e3f06c5f46b281f9049ae884c73130
Oliver <oliver.gruendel2004@gmail.com>
2023-11-27 12:37:46 +0100

    Portfolio II Aufgabe 2

---
+++ AlgoDat/Portfolio II/Aufgabe_2.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(define (linker-index ziffer zahl)
+  (define (linker-index-iter ziffer zahl laenge r)
+    (if (= 0 zahl)
+        #f
+        (if (= ziffer (quotient zahl (expt 10 (- laenge 1))))
+            r
+            (linker-index-iter ziffer (remainder zahl (expt 10 (- laenge 1))) (- laenge 1) (+ r 1))))
+    )
+
+  (linker-index-iter ziffer zahl (laenge zahl 0) 1)
+  )
+
+(define (laenge zahl l)
+  (if (= zahl 0)
+      l
+      (laenge (quotient zahl 10) (+ l 1)))
+  )
