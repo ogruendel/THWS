7440e851598b1df042c0cbd13958c976c0ef4cba
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-11-29 18:14:27 +0100

    Chapter 9: Auto Konstruktor, Setter

---
+++ Programmieren_1/src/chapter9/Main.java
@@ -0,0 +1,9 @@
+package chapter9;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto golf = new Auto("VW", "blau", 1500f);
+        Auto a1 = new Auto("Audi", "gelb", 200f);
+        Auto corsa = new Auto("Opel", "rot", 1200f);
+    }
+}
