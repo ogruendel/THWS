43a241c4f1dc9a9fd247c70f23f943e7faabb900
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-07 21:47:12 +0100

    Uhr

--- Programmieren_1/src/chapter10/Main.java
+++ Programmieren_1/src/chapter10/Main.java
@@ -1,10 +1,8 @@
 package chapter10;
 
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
+        // Aufgabe 1
         Raum i215 = new Raum("I.2.15", 32, false);
         Raum i21 = new Raum("I.2.1", 40, false);
 
@@ -20,5 +18,37 @@ public class Main {
         i21.alleStudenten();
         i21.verlasseRaum(student1);
         i21.alleStudenten();
+
+        // Aufgabe 2
+        Uhr[] weltzeit = new Uhr[]{
+                new Uhr(0, 23, 7),
+                new Uhr(1, 23, 7),
+                new Uhr(2, 23, 7),
+                new Uhr(3, 23, 7),
+                new Uhr(4, 23, 7),
+                new Uhr(5, 23, 7),
+                new Uhr(6, 23, 7),
+                new Uhr(7, 23, 7),
+                new Uhr(8, 23, 7),
+                new Uhr(9, 23, 7),
+                new Uhr(10, 23, 7),
+                new Uhr(11, 23, 7),
+                new Uhr(12, 23, 7),
+                new Uhr(13, 23, 7),
+                new Uhr(14, 23, 7),
+                new Uhr(15, 23, 7),
+                new Uhr(16, 23, 7),
+                new Uhr(17, 23, 7),
+                new Uhr(18, 23, 7),
+                new Uhr(19, 23, 7),
+                new Uhr(20, 23, 7),
+                new Uhr(21, 23, 7),
+                new Uhr(22, 23, 7),
+                new Uhr(23, 23, 7)
+        };
+
+        for (int i = 0; i < weltzeit.length; i++) {
+            weltzeit[i].naechsteSekunde();
+        }
     }
 }
---
+++ Programmieren_1/src/chapter10/Uhr.java
@@ -0,0 +1,55 @@
+package chapter10;
+
+public class Uhr {
+    private int stunde;
+    private int minute;
+    private int sekunde;
+
+    public Uhr(int stunde, int minute, int sekunde) {
+        if (stunde >= 24 || minute >= 60 || sekunde >= 60) {
+            this.stunde = 12;
+            this.minute = 0;
+            this.sekunde = 0;
+        } else {
+            this.stunde = stunde;
+            this.minute = minute;
+            this.sekunde = sekunde;
+        }
+    }
+
+    public int getStunde() {
+        return this.stunde;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+    public int getSekunde() {
+        return this.sekunde;
+    }
+
+    public void setUhr(int stunde, int minute, int sekunde) {
+        if (stunde >= 24) {
+            System.out.println("Stunde mehr als 24");
+        } else {
+            this.stunde = stunde;
+        }
+
+        if (minute >= 60) {
+            System.out.println("Minute mehr als 60");
+        } else {
+            this.minute = minute;
+        }
+
+        if (sekunde >= 60) {
+            System.out.println("Sekunde mehr als 60");
+        } else {
+            this.sekunde = sekunde;
+        }
+    }
+
+    public void naechsteSekunde() {
+        this.sekunde++;
+    }
+}
