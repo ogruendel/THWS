3df9bb36a0312c1738e255b1d27eb0dd98ca680b
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-12 19:43:08 +0100

    Betrag

---
+++ Programmieren_1/src/chatper11/Betrag.java
@@ -0,0 +1,22 @@
+package chatper11;
+
+public class Betrag {
+    public static int abs(int x) {
+        return x < 0 ? x * -1 : x;
+    }
+    public static float abs(float x) {
+        return x < 0 ? x * -1 : x;
+    }
+
+    public static double abs(double x) {
+        return x < 0 ? x * -1 : x;
+    }
+
+    public static short abs(short x) {
+        return x < 0 ? (short) (x * -1) : x;
+    }
+
+    public static long abs(long x) {
+        return x < 0 ? x * -1 : x;
+    }
+}
