af05e15e464b2ace4ae8c90b97eb79f0767d33e7
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-12 20:18:46 +0100

    Tage im Monat

---
+++ Programmieren_1/src/chatper11/TageImMonat.java
@@ -0,0 +1,34 @@
+package chatper11;
+
+import java.util.Scanner;
+
+public class TageImMonat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Monat: ");
+        String input = scanner.nextLine();
+        boolean fertig = false;
+
+        do {
+            try {
+                System.out.println(tageImMonat(input));
+                fertig = true;
+            } catch (RuntimeException e) {
+                System.out.println("Neuer Monat: ");
+                input = scanner.nextLine();
+            }
+        } while (!fertig);
+
+        scanner.close();
+    }
+
+    public static int tageImMonat(String monat) {
+        return switch (monat) {
+            case "Februar" -> 28;
+            case "April", "Juni", "September", "November" -> 30;
+            case "Januar", "März", "Mai", "Juli", "August", "Oktober", "Dezember" -> 31;
+            default -> throw new RuntimeException("falscher Monat");
+        };
+    }
+}
