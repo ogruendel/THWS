a13a93787b834b09f8f451730f801059e0f5a43a
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-12 16:25:56 +0100

    3D Tic Tac Toe

---
+++ Programmieren_1/src/chatper11/TicTacToe3D.java
@@ -0,0 +1,38 @@
+package chatper11;
+
+
+public class TicTacToe3D {
+    public static void main(String[] args) {
+        boolean[][][] game = new boolean[3][3][3];
+        spielen(game);
+        drucken(game);
+    }
+
+    public static void spielen(boolean[][][] feld) {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    int rand = (int) (Math.random() * 2);
+
+                    feld[x][y][z] = rand != 0;
+                }
+            }
+        }
+    }
+
+    public static void drucken(boolean[][][] feld) {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    if (feld[x][y][z]) {
+                        System.out.print("x ");
+                    } else {
+                        System.out.print("o ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
