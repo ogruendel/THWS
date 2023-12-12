a6e3bc5d5243102e74a6b375f625af2a02156f60
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-12 16:34:15 +0100

    Zeilenmaximum

---
+++ Programmieren_1/src/chatper11/ZeilenMaxima.java
@@ -0,0 +1,16 @@
+package chatper11;
+
+public class ZeilenMaxima {
+    public static double[] zeilenMaxima(double[][] input) {
+        double[] output = new double[input.length];
+        for (int i = 0; i < input.length; i++) {
+            double max = 0;
+            for (int j = 0; j < input[i].length; j++) {
+                max = max < input[i][j] ? input[i][j] : max;
+            }
+            output[i] = max;
+        }
+
+        return output;
+    }
+}
