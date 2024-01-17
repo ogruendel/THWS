35d26ffd506493475c04742fe4d5fe31516acf6b
Oliver Gründel <oliver.gruendel2004@gmail.com>
2024-01-17 19:03:17 +0100

    Zaehle Farbe

--- AlgoDat/Blatt_13/Mastermind.rkt
+++ AlgoDat/Blatt_13/Mastermind.rkt
@@ -8,3 +8,17 @@
       (zaehleSchwarz (cdr code) (cdr guess)))
     )
   )
+
+(define (zaheleFarbe code farbe)
+  (if (empty? code)
+    0
+    (if (= (car code) farbe)
+      (+ 1 (zaheleFarbe (cdr code) farbe))
+      (zaheleFarbe (cdr code) farbe))
+    )
+  )
+
+(zaheleFarbe '(1 2 3 4) 1)
+(zaheleFarbe '(1 1 2 2) 1)
+(zaheleFarbe '(1 2 3 4) 5)
+(zaheleFarbe '(5 2 3 5) 5)
